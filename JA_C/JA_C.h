33acd77f917eeec40b1ee4d0686620a956b1a2be
Mycelion <filipmorchat@interia.pl>
2025-01-18 15:39:49 +0100

    kontynuacja prac nad projektem

--- JA_C/JA_C.h
+++ JA_C/JA_C.h
@@ -5,3 +5,10 @@
 #else
 #define JA_C_API __declspec(dllimport)
 #endif
+
+#include <string>
+#include <fstream>
+#include <iostream>
+#include <vector>
+
+extern "C" JA_C_API int test(int a, int b);
