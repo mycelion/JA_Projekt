785c20ef27727e8ceb3151cdec1369a99f471133
Mycelion <filipmorchat@interia.pl>
2025-02-05 20:53:22 +0100

    yet another małe zmiany

--- JA_Asm/JA_Asm.asm
+++ JA_Asm/JA_Asm.asm
@@ -28,17 +28,21 @@ ApplyVignette PROC
     push r15
     sub rsp, 32
 
-    ; Save parameters correctly with zero extension
-    movsxd rdx, edx         ; Zero-extend image width to 64 bits
+    ; Save parameters with zero extension
+    movsxd rdx, edx         ; ImageWidth
     mov [ImageWidth], rdx
-    movsxd r8, r8d          ; Zero-extend image height
+    movsxd r8, r8d          ; ImageHeight
     mov [ImageHeight], r8
-    movsxd r9, r9d          ; Zero-extend image stride
+    movsxd r9, r9d          ; ImageStride
     mov [ImageStride], r9
     movsd [MaxRadius], xmm0
+
+    ; Clamp Power between 0.0 and 1.0 before storing
+    vmaxsd xmm1, xmm1, [ZERO]
+    vminsd xmm1, xmm1, [ONE]
     movsd [Power], xmm1
 
-    ; Calculate center coordinates using stored dimensions
+    ; Calculate center coordinates
     mov rdx, [ImageWidth]
     vcvtsi2sd xmm0, xmm0, rdx
     vdivsd xmm0, xmm0, qword ptr [TWO]
@@ -63,41 +67,39 @@ process_pixels:
     cmp r15, [ImageWidth]
     jge next_row
 
-    ; Calculate byte offset for current pixel (3 bytes per pixel)
+    ; Calculate byte offset for current pixel
     mov rbx, r15
     imul rbx, 3
 
     ; Calculate dx = (x_pixel - CenterX)
-    vcvtsi2sd xmm0, xmm0, r15    ; x_pixel is r15
+    vcvtsi2sd xmm0, xmm0, r15
     vsubsd xmm0, xmm0, [CenterX]
     vmulsd xmm0, xmm0, xmm0      ; dx^2
 
     ; Calculate dy = (y_pixel - CenterY)
-    vcvtsi2sd xmm1, xmm1, r12    ; current row (y_pixel)
+    vcvtsi2sd xmm1, xmm1, r12
     vsubsd xmm1, xmm1, [CenterY]
     vmulsd xmm1, xmm1, xmm1      ; dy^2
 
     vaddsd xmm0, xmm0, xmm1      ; dx^2 + dy^2
     vsqrtsd xmm0, xmm0, xmm0     ; distance
 
-    ; Normalize distance by MaxRadius
+    ; Normalize distance by MaxRadius and multiply by clamped Power
     vdivsd xmm0, xmm0, [MaxRadius]
+    vmulsd xmm0, xmm0, [Power]   ; Apply Power scaling
 
-    vmovsd xmm1, [Power]
-    call pow_simulated            ; Custom function to compute xmm0^Power
-
-    ; Calculate vignette factor: max(0, 1 - (distance^Power))
-    vmovsd xmm2, [ONE]
-    vsubsd xmm0, xmm2, xmm0       ; 1 - (distance^Power)
-    vmaxsd xmm0, xmm0, [ZERO]     ; Clamp to 0
-    vminsd xmm0, xmm0, [ONE]      ; Clamp to 1
+    ; Calculate vignette factor: max(0, 1 - (distance * Power))
+    vmovsd xmm1, [ONE]
+    vsubsd xmm0, xmm1, xmm0      ; 1 - (distance * Power)
+    vmaxsd xmm0, xmm0, [ZERO]    ; Clamp to 0
+    vminsd xmm0, xmm0, [ONE]     ; Clamp to 1
 
     ; Process BGR pixels
     movzx eax, byte ptr [r14 + rbx]      ; Blue
     movzx edx, byte ptr [r14 + rbx + 1]  ; Green
     movzx r8d, byte ptr [r14 + rbx + 2]  ; Red
 
-    ; Apply vignette factor to each channell
+    ; Apply vignette factor
     vcvtsi2sd xmm1, xmm1, eax
     vmulsd xmm1, xmm1, xmm0
     vcvtsd2si eax, xmm1
@@ -111,16 +113,15 @@ process_pixels:
     vcvtsd2si r8d, xmm1
 
     ; Store results
-    mov byte ptr [r14 + rbx], al        ; Blue
-    mov byte ptr [r14 + rbx + 1], dl    ; Green
-    mov byte ptr [r14 + rbx + 2], r8b   ; Red
+    mov byte ptr [r14 + rbx], al
+    mov byte ptr [r14 + rbx + 1], dl
+    mov byte ptr [r14 + rbx + 2], r8b
 
-    ; Move to next pixel
     inc r15
     jmp process_pixels
 
 next_row:
-    add r14, [ImageStride]   ; Move to next row
+    add r14, [ImageStride]
     inc r12
     jmp process_rows
 
@@ -132,10 +133,6 @@ done:
     pop r12
     pop rbx
     pop rbp
-    ret
-
-pow_simulated:
-    vmulsd xmm0, xmm0, xmm0
     ret
 
 ApplyVignette ENDP
